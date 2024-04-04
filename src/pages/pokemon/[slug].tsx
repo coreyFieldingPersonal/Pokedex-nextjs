c1b815d09fef0ff6c7e14ba2d06f0e7387a4edec
Corey Fielding <>
2024-04-04 22:28:14 +0100

    type fix

--- src/pages/pokemon/[slug].tsx
+++ src/pages/pokemon/[slug].tsx
@@ -1,7 +1,7 @@
 // import { Tooltip } from "@/components"
 import { useRouter } from "next/router"
 import Image from "next/image"
-import { Suspense, useState } from "react"
+import { useState } from "react"
 import { LeftArrowIcon } from "@/components/Icons/LeftArrowIcon"
 
 export const getStaticPaths = async () => {
@@ -59,83 +59,78 @@ export default function Page({ data }: any) {
 
   const router = useRouter()
 
-  if (router.isFallback) {
-    return <div>Catching pokemon...</div>
-  } else
-    return (
-      <Suspense>
-        <div className="font-mono px-6 lg:px-0 max-w-7xl mx-auto 2xl:max-w-7xl py-20">
-          <button
-            type="button"
-            onClick={() => router.back()}
-            className="flex gap-x-4 items-center"
-          >
-            <LeftArrowIcon />
-            Back to Pokedex
-          </button>
-          <div className="flex flex-col lg:flex-row gap-6">
-            <div className="border-gray-300/80 mt-10 backdrop-blur-2xl border bg-zinc-300/10 rounded-lg p-5">
-              <div className="flex gap-x-8 items-end">
-                <Image
-                  src={sprites.front_shiny}
-                  width={150}
-                  height={150}
-                  alt={name ?? ""}
-                />
-                {name && (
-                  <h1 className="text-5xl uppercase flex flex-col items-center gap-4">
-                    {name}
-                  </h1>
-                )}
-              </div>
-              <div className="flex gap-x-3 mt-5 justify-end">
-                {types &&
-                  types?.length > 0 &&
-                  types?.map(
-                    ({ type }: { type: { name: string } }, idx: number) => (
-                      <TypeBadge key={`${type.name}-${idx}`} type={type.name} />
-                    )
-                  )}
-                {/* <Tooltip open={open} onOpenChange={setOpen}>
+  return (
+    <div className="font-mono px-6 lg:px-0 max-w-7xl mx-auto 2xl:max-w-7xl py-20">
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="flex gap-x-4 items-center"
+      >
+        <LeftArrowIcon />
+        Back to Pokedex
+      </button>
+      <div className="flex flex-col lg:flex-row gap-6">
+        <div className="border-gray-300/80 mt-10 backdrop-blur-2xl border bg-zinc-300/10 rounded-lg p-5">
+          <div className="flex gap-x-8 items-end">
+            <Image
+              src={sprites.front_shiny}
+              width={150}
+              height={150}
+              alt={name ?? ""}
+            />
+            {name && (
+              <h1 className="text-5xl uppercase flex flex-col items-center gap-4">
+                {name}
+              </h1>
+            )}
+          </div>
+          <div className="flex gap-x-3 mt-5 justify-end">
+            {types &&
+              types?.length > 0 &&
+              types?.map(
+                ({ type }: { type: { name: string } }, idx: number) => (
+                  <TypeBadge key={`${type.name}-${idx}`} type={type.name} />
+                )
+              )}
+            {/* <Tooltip open={open} onOpenChange={setOpen}>
             <span className="px-5 text-white bg-green-400/40 border-green-500 py-2 flex w-fit text-sm font-bold justify-center border rounded-md backdrop-blur-2xl  lg:static ">
               Grass
             </span>
           </Tooltip> */}
-              </div>
-              <div className="mt-4">
-                <div className="text-lg border-b flex justify-between py-1">
-                  <span>Height:</span> <span>{height} m</span>
-                </div>
-                <div className="text-lg border-b flex justify-between py-1">
-                  <span>Weight:</span> <span>{weight} lbs</span>
-                </div>
-              </div>
+          </div>
+          <div className="mt-4">
+            <div className="text-lg border-b flex justify-between py-1">
+              <span>Height:</span> <span>{height} m</span>
             </div>
-            <div className="border-gray-300/80 mt-10 backdrop-blur-2xl border bg-zinc-300/10 rounded-lg p-5 flex-1">
-              {stats &&
-                stats.length > 0 &&
-                stats?.map(
-                  ({ stat }: { stat: { name: string } }, idx: number) => (
-                    <div
-                      key={idx}
-                      className="uppercase flex justify-between w-full gap-6 border-b my-3 items-center"
-                    >
-                      <span>{stat?.name.split("-").join(" ")}</span>
-                      <span>{stats[idx]["base_stat"]}</span>
-                    </div>
-                  )
-                )}
+            <div className="text-lg border-b flex justify-between py-1">
+              <span>Weight:</span> <span>{weight} lbs</span>
             </div>
           </div>
-          {abilities.map(({ ability, idx }) => (
-            <div
-              key={idx}
-              className="border-gray-300/80 mt-10 backdrop-blur-2xl border bg-zinc-300/10 rounded-lg p-5 flex-1"
-            >
-              {ability.name}
-            </div>
-          ))}
         </div>
-      </Suspense>
-    )
+        <div className="border-gray-300/80 mt-10 backdrop-blur-2xl border bg-zinc-300/10 rounded-lg p-5 flex-1">
+          {stats &&
+            stats.length > 0 &&
+            stats?.map(({ stat }: { stat: { name: string } }, idx: number) => (
+              <div
+                key={idx}
+                className="uppercase flex justify-between w-full gap-6 border-b my-3 items-center"
+              >
+                <span>{stat?.name.split("-").join(" ")}</span>
+                <span>{stats[idx]["base_stat"]}</span>
+              </div>
+            ))}
+        </div>
+      </div>
+      {abilities.map(
+        ({ ability }: { ability: { name: string } }, idx: number) => (
+          <div
+            key={idx}
+            className="border-gray-300/80 mt-10 backdrop-blur-2xl border bg-zinc-300/10 rounded-lg p-5 flex-1"
+          >
+            {ability.name}
+          </div>
+        )
+      )}
+    </div>
+  )
 }
